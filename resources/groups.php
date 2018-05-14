662e5542baa5387514f7fc8c805ab2bdd92fab6a
ShrimpDevs <ShrimpDevs@github.com>
2018-05-14 17:04:52 +0300

    notificationUpdate
    add:
    -notification sending for studens\groups

--- resources/groups.php
+++ resources/groups.php
@@ -3,70 +3,32 @@ session_start();
 if(!isset($_SESSION['Name'])) header("Location: auth.php"); 
 ?>
 
-<!doctype html>
+<!DOCTYPE html>
 <html lang="ru">
-  <head>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="Список групп, с которыми работает бот.">
-    <meta name="author" content="">
-    <link rel="icon" href="../../../../favicon.ico">
+    <title>Document</title>
+</head>
+<body>
+    <form method="post">
+    <div class="header">
+        <div class="part"> </div>
+        <div class="searchbox">
+            <textarea name="search" rows="1" wrap="off" placeholder="поиск" class="search" onkeypress='search()'></textarea>
+        </div>
+    </div>
+    <div class="navigation">
+        <div class="selected_menu" id="menu_groups" onclick="location.href='groups.php';">группы</div>
+        <div class="menu" id="menu_students" onclick="location.href='students.php';">студенты</div>
+        <div class="menu" id="menu_search" onclick="location.href='search.php';">поиск</div>
+        <div class="menu" id="menu_control" onclick="location.href='control.php';">панель управления</div>
+    </div>
 
-    <title>Список групп</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="../../../../dist/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="album.css" rel="stylesheet">
-  </head>
-
-  <body>
-    <header>
-      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <a class="navbar-brand" href="#">Московский Политех</a>
-
-  <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
-    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-      <li class="nav-item ">
-        <a class="nav-link active" href="groups.php">Группы</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="students.php">Студенты<span class="sr-only">(current)</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="control.php">Панель Управления</a>
-      </li>
-      <li class="nav-item">
-        <form method="post">
-          <input type="search" class="form-control mr-sm-2" name="exit">
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
-      </button>
-        </form>
-      </li>
-    </ul>
-    <form action="search.php" method="get" class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" aria-label="Search" name = "word">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Поиск
-      </button>
-    </form>
-  </div>
-</nav>
-    </header>
-  <body>
-
-<div class="container-fluid">
-  <div class="row">
-    <div class="col-sm-3">
-      <?php
+    <div class="column1"> 
+    <?php
     $sort=0;
     try
     {
@@ -83,7 +45,7 @@ if(!isset($_SESSION['Name'])) header("Location: auth.php");
             echo '<div class="description">'.$post["description"].'</div>';
             echo '</div>';
             echo '<div class="checkbox">';
-            echo '<input type="checkbox" class="check" name="" id="">';
+            echo '<input type="checkbox" class="check" name="targetGroups[]" value="'.$post["group_num"].'">';
             echo '</div>';
             echo '</div>';
         }
@@ -98,8 +60,9 @@ if(!isset($_SESSION['Name'])) header("Location: auth.php");
     }?>
 
     </div>
-    <div class="col-sm-3">
-       <?php
+
+    <div class="column2"> 
+    <?php
     $sort=0;
     try
     {
@@ -116,7 +79,7 @@ if(!isset($_SESSION['Name'])) header("Location: auth.php");
             echo '<div class="description">'.$post["description"].'</div>';
             echo '</div>';
             echo '<div class="checkbox">';
-            echo '<input type="checkbox" class="check" name="" id="">';
+            echo '<input type="checkbox" class="check" name="targetGroups[]" value="'.$post["group_num"].'">';
             echo '</div>';
             echo '</div>';
         }
@@ -130,8 +93,9 @@ if(!isset($_SESSION['Name'])) header("Location: auth.php");
         die();
     }?>
     </div>
-    <div class="col-sm-3">
-      <?php
+
+    <div class="column3"> 
+    <?php
     $sort=0;
     try
     {
@@ -148,7 +112,7 @@ if(!isset($_SESSION['Name'])) header("Location: auth.php");
             echo '<div class="description">'.$post["description"].'</div>';
             echo '</div>';
             echo '<div class="checkbox">';
-            echo '<input type="checkbox" class="check" name="" id="">';
+            echo '<input type="checkbox" class="check" name="targetGroups[]" value="'.$post["group_num"].'">';
             echo '</div>';
             echo '</div>';
         }
@@ -162,21 +126,13 @@ if(!isset($_SESSION['Name'])) header("Location: auth.php");
         die();
     }?>
     </div>
-    <div class="col-md-3">
+
+    <div class="column4">  
         <textarea class="textarea" name="text" cols="30" rows="10" placeholder="текст уведомления"></textarea>
-        <input type="submit" class="send" value="Отправить">
-        <!--
-        <form method="post">
-            <input type="submit" class="send" name="exit" value="Выход">
-        </form>
-      -->
+        <input type="submit" class="send" name="send" value="send">
+            <input type="submit" class="send" name="exit" value="выход">
     </div>
-  </div>
-</div>
-
-
-
-    
+    </form>
     <!-- немного js  -->
     <script>
         function search() 
@@ -192,6 +148,55 @@ if(!isset($_SESSION['Name'])) header("Location: auth.php");
 </html>
 
 <?php 
+    if(isset($_POST['send']))
+    {
+        $author = "admin";
+        $text = $_POST['text'];
+        $targets = $_POST['targetGroups'];
+        $preparedTargets = implode(",",$targets);
+        $isSent = 0;
+
+        if(empty($text))
+        {
+            echo "<script>location.replace('')</script>";
+        }
+        else
+        {
+            if(stristr($content, '!important') === FALSE)
+            {
+            $dbHost = "std-mysql";
+            $dbUser = "std_320";
+            $dbPass = "meowmeow";
+            $dbName = "std_320";
+
+
+            if (mysqli_connect_errno()) {
+                exit();
+            }
+        
+            echo '<br>';
+
+            $mysqli = new mysqli($dbHost, $dbUser, $dbPass, $dbName);
+        
+            $query = "INSERT INTO `notifications` (`author`, `text`, `target`, `is_sent`) 
+                VALUES ('$author', '$text', '$preparedTargets', '$isSent');";
+            
+            if ($result = $mysqli->query($query)) 
+            {
+            }
+
+            $text= "";
+
+            $mysqli->close();
+            echo "<script>location.replace('')</script>";
+        }
+        else
+        {
+            echo "<script>location.replace('')</script>";
+        }
+        }
+    }
+
     if(isset($_POST['exit'])) 
     { 
         session_destroy(); 
@@ -199,3 +204,4 @@ if(!isset($_SESSION['Name'])) header("Location: auth.php");
         exit; 
     } 
 ?>
+
--- resources/students.php
+++ resources/students.php
@@ -3,69 +3,30 @@ session_start();
 if(!isset($_SESSION['Name'])) header("Location: auth.php"); 
 ?>
 
-<!doctype html>
+<!DOCTYPE html>
 <html lang="ru">
-  <head>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="author" content="">
-    <link rel="icon" href="../../../../favicon.ico">
-
-    <title>Студенты</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="../../../../dist/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="album.css" rel="stylesheet">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="style.css">
-  </head>
-
-
-    <header>
-      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <a class="navbar-brand" href="#">Московский Политех</a>
-
-  <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
-    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-      <li class="nav-item ">
-        <a class="nav-link" href="groups.php">Группы <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link active" href="students.php">Студенты<span class="sr-only">(current)</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="control.php">Панель Управления</a>
-      </li>
-      <li class="nav-item">
-        <form method="post">
-          <input type="submit" class="send ex nav-link" name="exit" value="Выход">
-
-        </form>
-      </li>
-    </ul>
-   <form action="search.php" method="get" class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" aria-label="Search" name = "word">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Поиск
-      </button>
-    </form>
-  </div>
-</nav>
-    </header>
-
-</span>
+    <title>Document</title>
+</head>
 <body>
-
-<div class="container-fluid">
-  <div class="row">
-    <div class="col-sm">
+    <form method='post'>
+    <div class="header">
+        <div class="part"> </div>
+        <div class="searchbox">
+            <textarea name="search" rows="1" wrap="off" placeholder="поиск" class="search" onkeypress='search()'></textarea>
+        </div>
+    </div>
+    <div class="navigation">
+        <div class="menu" id="menu_groups" onclick="location.href='groups.php';">группы</div>
+        <div class="selected_menu" id="menu_students" onclick="location.href='students.php';">студенты</div>
+        <div class="menu" id="menu_search" onclick="location.href='search.php';">поиск</div>
+        <div class="menu" id="menu_control" onclick="location.href='control.php';">панель управления</div>
+    </div>
+    <div class="column1"> 
     <?php
     $sort=0;
     try
@@ -87,7 +48,7 @@ if(!isset($_SESSION['Name'])) header("Location: auth.php");
                     echo '<div class="description">'.$post["group_num"].'</div>';
                     echo '</div>';
                     echo '<div class="checkbox">';
-                    echo '<input type="checkbox" class="check" name="" id="">';
+                    echo '<input type="checkbox" class="check" name="targetStudents[]" value="'.$post["vk_id"].'">';
                     echo '</div>';
                     echo '</div>';
                 }
@@ -105,7 +66,7 @@ if(!isset($_SESSION['Name'])) header("Location: auth.php");
                 echo '<div class="description">'.$post["group_num"].'</div>';
                 echo '</div>';
                 echo '<div class="checkbox">';
-                echo '<input type="checkbox" class="check" name="" id="">';
+                echo '<input type="checkbox" class="check" name="targetStudents[]" value="'.$post["vk_id"].'">';
                 echo '</div>';
                 echo '</div>';
             }
@@ -122,7 +83,7 @@ if(!isset($_SESSION['Name'])) header("Location: auth.php");
 
     </div>
 
-  <div class="col-sm">
+    <div class="column2"> 
     <?php
     $sort=0;
     try
@@ -144,7 +105,7 @@ if(!isset($_SESSION['Name'])) header("Location: auth.php");
                     echo '<div class="description">'.$post["group_num"].'</div>';
                     echo '</div>';
                     echo '<div class="checkbox">';
-                    echo '<input type="checkbox" class="check" name="" id="">';
+                    echo '<input type="checkbox" class="check" name="targetStudents[]" value="'.$post["vk_id"].'">';
                     echo '</div>';
                     echo '</div>';
                 }
@@ -162,7 +123,7 @@ if(!isset($_SESSION['Name'])) header("Location: auth.php");
                 echo '<div class="description">'.$post["group_num"].'</div>';
                 echo '</div>';
                 echo '<div class="checkbox">';
-                echo '<input type="checkbox" class="check" name="" id="">';
+                echo '<input type="checkbox" class="check" name="targetStudents[]" value="'.$post["vk_id"].'">';
                 echo '</div>';
                 echo '</div>';
             }
@@ -178,7 +139,7 @@ if(!isset($_SESSION['Name'])) header("Location: auth.php");
     }?>
     </div>
 
-   <div class="col-sm">
+    <div class="column3"> 
     <?php
     $sort=0;
     try
@@ -200,7 +161,7 @@ if(!isset($_SESSION['Name'])) header("Location: auth.php");
                     echo '<div class="description">'.$post["group_num"].'</div>';
                     echo '</div>';
                     echo '<div class="checkbox">';
-                    echo '<input type="checkbox" class="check" name="" id="">';
+                    echo '<input type="checkbox" class="check" name="targetStudents[]" value="'.$post["vk_id"].'">';
                     echo '</div>';
                     echo '</div>';
                 }
@@ -218,7 +179,7 @@ if(!isset($_SESSION['Name'])) header("Location: auth.php");
                 echo '<div class="description">'.$post["group_num"].'</div>';
                 echo '</div>';
                 echo '<div class="checkbox">';
-                echo '<input type="checkbox" class="check" name="" id="">';
+                echo '<input type="checkbox" class="check" name="targetStudents[]" value="'.$post["vk_id"].'">';
                 echo '</div>';
                 echo '</div>';
             }
@@ -234,12 +195,11 @@ if(!isset($_SESSION['Name'])) header("Location: auth.php");
     }?>
     </div>
 
-<div class="col-sm">
+    <div class="column4">  
         <textarea class="textarea" name="text" cols="30" rows="10" placeholder="текст уведомления"></textarea>
-        <input type="button" name="send" class="send" value="Отправить">   
-         
-
+        <input type="submit" name="send" class="send" value="send">    
     </div>
+    </form>  
     <!-- немного js  -->
     <script>
         function search() 
@@ -250,14 +210,57 @@ if(!isset($_SESSION['Name'])) header("Location: auth.php");
             }
         }
     </script>
-    <!--  -->
+    <!--  -->  
 </body>
 </html>
-<?php 
-    if(isset($_POST['exit'])) 
-    { 
-        session_destroy(); 
-        header('Location: /auth.php'); 
-        exit; 
-    } 
+
+<?php
+    if(isset($_POST['send']))
+    {
+        $author = "admin";
+        $text = $_POST['text'];
+        $targets = $_POST['targetStudents'];
+        $preparedTargets = implode(",",$targets);
+        $isSent = 0;
+
+        if(empty($text))
+        {
+            echo "<script>location.replace('')</script>";
+        }
+        else
+        {
+            if(stristr($content, '!important') === FALSE)
+            {
+            $dbHost = "std-mysql";
+            $dbUser = "std_320";
+            $dbPass = "meowmeow";
+            $dbName = "std_320";
+
+
+            if (mysqli_connect_errno()) {
+                exit();
+            }
+        
+            echo '<br>';
+
+            $mysqli = new mysqli($dbHost, $dbUser, $dbPass, $dbName);
+        
+            $query = "INSERT INTO `notifications` (`author`, `text`, `target`, `is_sent`) 
+                VALUES ('$author', '$text', '$preparedTargets', '$isSent');";
+
+            if ($result = $mysqli->query($query)) 
+            {
+            }
+
+            $text= "";
+
+            $mysqli->close();
+            echo "<script>location.replace('')</script>";
+        }
+        else
+        {
+            echo "<script>location.replace('')</script>";
+        }
+        }
+    }
 ?>
