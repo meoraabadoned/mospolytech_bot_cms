63c522a0e9f53012221cb008a947b72ece8c67b4
yudemin <33002472+yudemin@users.noreply.github.com>
2018-05-18 22:34:14 +0300

    Add files via upload
    -optimizationUpdate
    -visual fix

--- resources/groups.php
+++ resources/groups.php
@@ -6,7 +6,7 @@ if(!isset($_SESSION['Name'])) header("Location: auth.php");
 <!doctype html>
 <html lang="ru">
   <head>
-    <link rel="stylesheet" href="style.css">
+   
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
@@ -23,7 +23,7 @@ if(!isset($_SESSION['Name'])) header("Location: auth.php");
     <link href="../../../../dist/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
-    <link href="album.css" rel="stylesheet">
+     <link rel="stylesheet" href="style.css">
   </head>
 
   <body>
@@ -46,10 +46,11 @@ if(!isset($_SESSION['Name'])) header("Location: auth.php");
         <a class="nav-link" href="control.php">Панель Управления</a>
       </li>
       <li class="nav-item">
+        <a class="nav-link" href="question.php">Вопросы</a>
+      </li>
+      <li class="nav-item">
         <form method="post">
-          <input type="search" class="form-control mr-sm-2" name="exit">
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
-      </button>
+          <input type="submit" class="send ex nav-link" name="exit" value="Выход">
         </form>
       </li>
     </ul>
@@ -65,8 +66,8 @@ if(!isset($_SESSION['Name'])) header("Location: auth.php");
 
 <div class="container-fluid">
   <div class="row">
-    <div class="col-sm-3">
-      <?php
+    <div class="col-sm">
+     <?php
     $sort=0;
     try
     {
@@ -83,7 +84,7 @@ if(!isset($_SESSION['Name'])) header("Location: auth.php");
             echo '<div class="description">'.$post["description"].'</div>';
             echo '</div>';
             echo '<div class="checkbox">';
-            echo '<input type="checkbox" class="check" name="" id="">';
+            echo '<input type="checkbox" class="check" name="targetGroups[]" value="'.$post["group_num"].'">';
             echo '</div>';
             echo '</div>';
         }
@@ -96,10 +97,10 @@ if(!isset($_SESSION['Name'])) header("Location: auth.php");
         print "Error!: " . $e->getMessage() . "<br/>";
         die();
     }?>
-
     </div>
-    <div class="col-sm-3">
-       <?php
+    
+    <div class="col-sm">
+    <?php
     $sort=0;
     try
     {
@@ -116,7 +117,7 @@ if(!isset($_SESSION['Name'])) header("Location: auth.php");
             echo '<div class="description">'.$post["description"].'</div>';
             echo '</div>';
             echo '<div class="checkbox">';
-            echo '<input type="checkbox" class="check" name="" id="">';
+            echo '<input type="checkbox" class="check" name="targetGroups[]" value="'.$post["group_num"].'">';
             echo '</div>';
             echo '</div>';
         }
@@ -130,7 +131,7 @@ if(!isset($_SESSION['Name'])) header("Location: auth.php");
         die();
     }?>
     </div>
-    <div class="col-sm-3">
+    <div class="col-sm">
       <?php
     $sort=0;
     try
@@ -148,7 +149,7 @@ if(!isset($_SESSION['Name'])) header("Location: auth.php");
             echo '<div class="description">'.$post["description"].'</div>';
             echo '</div>';
             echo '<div class="checkbox">';
-            echo '<input type="checkbox" class="check" name="" id="">';
+            echo '<input type="checkbox" class="check" name="targetGroups[]" value="'.$post["group_num"].'">';
             echo '</div>';
             echo '</div>';
         }
@@ -162,14 +163,9 @@ if(!isset($_SESSION['Name'])) header("Location: auth.php");
         die();
     }?>
     </div>
-    <div class="col-md-3">
+    <div class="col-md">
         <textarea class="textarea" name="text" cols="30" rows="10" placeholder="текст уведомления"></textarea>
         <input type="submit" class="send" value="Отправить">
-        <!--
-        <form method="post">
-            <input type="submit" class="send" name="exit" value="Выход">
-        </form>
-      -->
     </div>
   </div>
 </div>
@@ -190,7 +186,67 @@ if(!isset($_SESSION['Name'])) header("Location: auth.php");
     <!--  -->
 </body>
 </html>
-
+<?php 
+    if(isset($_POST['send']))
+    {
+        $author = "admin";
+        $text = $_POST['text'];
+        $targets = $_POST['targetGroups'];
+        $preparedTargets = implode(",",$targets);
+        $isSent = 0;
+        $type = "";
+        if($preparedTargets=="")
+        {
+            $type = "all";
+        }
+        else
+        {
+            $type = "groups";
+        }
+        if(empty($text))
+        {
+            echo "<script>location.replace('')</script>";
+        }
+        else
+        {
+            if(stristr($content, '!important') === FALSE)
+            {
+            $dbHost = "std-mysql";
+            $dbUser = "std_320";
+            $dbPass = "meowmeow";
+            $dbName = "std_320";
+            if (mysqli_connect_errno()) {
+                exit();
+            }
+        
+            echo '<br>';
+            $mysqli = new mysqli($dbHost, $dbUser, $dbPass, $dbName);
+        
+            $query = "INSERT INTO `notifications` (`author`, `text`,`target_type`, `target`, `is_sent`) 
+                VALUES ('$author', '$text', '$type' ,'$preparedTargets', '$isSent');";
+            
+            if ($result = $mysqli->query($query)) 
+            {
+            }
+            $text = "";
+            $type = "";
+            $preparedTargets ="";
+            $mysqli->close();
+            echo "<script>location.replace('http://sss.std-322.ist.mospolytech.ru/notification.php?place=groups')</script>";
+        }
+        else
+        {
+            echo "<script>location.replace('')</script>";
+        }
+        }
+    }
+    if(isset($_POST['exit'])) 
+    { 
+        session_destroy(); 
+        header('Location: /auth.php'); 
+        exit; 
+    } 
+?>
 <?php 
     if(isset($_POST['exit'])) 
     { 
